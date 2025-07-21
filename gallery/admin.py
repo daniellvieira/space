af5fcf0f57e06adb5837775b07cc2151a2ed0828
Daniel Vieira <danielvieiraufc@gmail.com>
2025-07-20 23:49:30 -0300

    feat: add category field to Photo model and update admin interface and templates

--- gallery/admin.py
+++ gallery/admin.py
@@ -3,10 +3,10 @@ from .models import Photo
 
 
 class ListPhoto(admin.ModelAdmin):
-    list_display = ('id', 'name', 'author', 'image_url')
+    list_display = ('id', 'name', 'author', 'image_url', 'category')
     list_display_links = ('id', 'name')
-    search_fields = ('name', 'author', 'description')
-    list_filter = ('author', 'description')
+    search_fields = ('name', 'author', 'description', 'category')
+    list_filter = ('author', 'description', 'category')
     list_per_page = 10
     list_max_show_all = 100
     list_editable = ('author', 'image_url')
--- gallery/models.py
+++ gallery/models.py
@@ -2,15 +2,20 @@ from django.db import models
 from django.conf import settings
 
 
+class CategoryOptions(models.IntegerChoices):
+    GALAXY = 1, 'Galáxia'
+    PLANET = 2, 'Planeta'
+    STAR = 3, 'Estrela'
+    OTHER = 4, 'Outro'
+
+
 class Photo(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
     author = models.CharField(max_length=150, null=False, blank=False)
     description = models.TextField(null=False, blank=False)
     image_url = models.CharField(max_length=255, null=False, blank=False)
+    category = models.IntegerField(
+        choices=CategoryOptions.choices, default=CategoryOptions.OTHER)
 
     def __str__(self):
-        return f'Photo:{self.name} from {self.description}'
-
-    @property
-    def get_image_full_path(self):
-        return f'{settings.MEDIA_URL}/{self.image_url}'
+        return f'Photo: [{self.get_category_display()}] {self.name} from {self.author}'
